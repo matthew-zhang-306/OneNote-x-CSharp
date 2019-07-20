6b7d3e6f45a2ef30fef3e4a7b2bbb8efd585f266
matthew-zhang-306 <matthew.zhang.306@gmail.com>
2019-07-20 11:14:51 -0500

    Initial Commit

---
+++ OneNote-x-CSharp/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Microsoft.Office.Interop.OneNote;
+
+namespace OneNote_x_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Load XML from OneNote data 
+             * This works
+             */
+            String strXML;
+            Application app = new Application();
+
+            app.GetHierarchy(null, HierarchyScope.hsPages, out strXML);
+            // Console.WriteLine(strXML);
+
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(strXML);
+            XmlElement hierarchy = xml.DocumentElement;
+
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xml.NameTable);
+            nsmgr.AddNamespace("one", "http://schemas.microsoft.com/office/onenote/2013/onenote");
+
+            Console.WriteLine(hierarchy.FirstChild.InnerXml);
+
+            string path = Directory.GetCurrentDirectory();
+            string combinedPath = Path.Combine(Directory.GetCurrentDirectory(), "dummy\\Dummy.txt");
+            string configPath = Path.Combine(Directory.GetCurrentDirectory(), "dummy\\config.txt");
+
+            Console.WriteLine(path);
+
+            /*
+             * Reads config file containing sensitive data
+             * This works 
+             */
+            string[] lines;
+            var list = new List<string>();
+            var fileStream = new FileStream(configPath, FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    list.Add(line);
+                }
+            }
+            lines = list.ToArray();
+
+            WinSCP.SessionOptions ops = new WinSCP.SessionOptions
+            {
+                Protocol = WinSCP.Protocol.Ftp,
+                HostName = lines[0],
+                UserName = lines[1],
+                Password = lines[2]
+            };
+
+            using (WinSCP.Session session = new WinSCP.Session())
+            {
+                session.Open(ops);
+
+                WinSCP.TransferOptions transferOptions = new WinSCP.TransferOptions();
+                transferOptions.TransferMode = WinSCP.TransferMode.Binary;
+
+                /*
+                 * Upload dummy file to the site
+                 * This works
+                 */
+                WinSCP.TransferOperationResult res = session.PutFiles(combinedPath, "/qlohome/qlodatabase/Reports/Dummy.txt", false, transferOptions);
+                res.Check();
+
+                foreach (WinSCP.TransferEventArgs transfer in res.Transfers)
+                {
+                    Console.WriteLine("Upload of {0} succeeded", transfer.FileName);
+                }
+
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# OneNote-x-CSharp
+ 
+A C# script for compiling reports of existing student work stored in OneNote. Creates reports in text form and in HTML form for use on a separate web service.
+
+For Gideon Math & Reading
+
+## Built With
+
+* [OneNote](https://docs.microsoft.com/en-us/office/client-developer/onenote/application-interface-onenote) - .NET APIs
+* [WinSCP](https://winscp.net/eng/index.php) - FTP
+
+## Authors
+
+* **Matthew Zhang** - [matthew-zhang-306](https://github.com/matthew-zhang-306)
+* **Sid Murali** - [vividsid94](https://github.com/vividsid94)
